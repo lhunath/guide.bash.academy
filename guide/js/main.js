4402b49bc6b6f60dad443416622fb0c4ea1bc0de
Maarten Billemont <lhunath@lyndir.com>
2014-04-22 13:52:02 -0400

    Inception - about bash, terminals, programs, file descriptors and streams.

--- guide/js/main.js
+++ guide/js/main.js
@@ -1,4 +1,24 @@
+function selectText(element) {
+    var doc = document, range, selection;    
+
+    if (doc.body.createTextRange) { //ms
+        range = doc.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) { //all others
+        selection = window.getSelection();        
+        range = doc.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
+
 $(function(){
     $.stellar();
+
+    $("kbd").on('click', function() {
+        selectText(this);
+    });
 });
 
