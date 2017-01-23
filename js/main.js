1d68ca8631a18a9fa5b3a8c37e43e0ab220d9aa1
Maarten Billemont <lhunath@lyndir.com>
2017-01-23 13:54:11 -0500

    Links for headings and paragraphs.

--- js/main.js
+++ js/main.js
@@ -15,30 +15,12 @@ function selectText(element) {
 }
 
 $(function(){
-    var currentChapter = $('body').attr('id');
-
-    // Make kbd clickable to select the text.
-    $('kbd').on('click', function() {
-        selectText(this);
-    });
-
-    // Build the ToC.
-    $('#toc').toc({
-        'selectors': 'section>h1,section>h2,section>h3'
-    });
-
-    // Make the current chapter active in the ToC.
-    $('nav li').each(function() {
-        var id = $(this).attr('id');
-        if (id) {
-            var idFields = id.split('_');
-            if (idFields[0] == 'chapter' && idFields[1] == currentChapter)
-                $(this).addClass('toc-active');
-        }
-    });
+    // Enable parallax backgrounds.
+    if( ! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
+        $.stellar();
 
     // Remember contribution.
-    if (localStorage.getItem("contributed")) {
+    if (localStorage.getItem('contributed')) {
         $('.contribute form').hide();
         $('.contribute .thanks').show();
     } else {
@@ -50,7 +32,7 @@ $(function(){
         $('.contribute .thanks').hide();
     });
     $('.contribute form').submit(function() {
-        localStorage.setItem("contributed", true);
+        localStorage.setItem('contributed', true);
     });
 
     // User's chapter completion checkboxes.
@@ -62,8 +44,49 @@ $(function(){
         localStorage.setItem($(this).attr('id'), this.checked);
     });
 
-    // Enable parallax backgrounds.
-    if( ! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
-        $.stellar();
+    // Build the ToC.
+    $('#toc').toc({
+        'selectors': 'section>h1,section>h2,section>h3'
+    });
+
+    // Make the current chapter active in the ToC.
+    var currentChapter = $('body').attr('id');
+    $('nav li').each(function() {
+        var id = $(this).attr('id');
+        if (id) {
+            var idFields = id.split('_');
+            if (idFields[0] == 'chapter' && idFields[1] == currentChapter)
+                $(this).addClass('toc-active');
+        }
+    });
+
+    // Make kbd clickable to select the text.
+    $('kbd').on('click', function() {
+        selectText(this);
+    });
+
+    // Add paragraph and heading links.
+    var h1 = 0, h2 = 0, p = 0;
+    $('section h1, section h2, section p').each(function() {
+        var id = '';
+        if ("h1" == this.tagName.toLowerCase()) {
+            ++h1;
+            h2 = 0;
+            p = 0;
+            id = 'h' + h1;
+        }
+        else if ("h2" == this.tagName.toLowerCase()) {
+            ++h2;
+            p = 0;
+            id = 'h' + h1 + '.' + h2;
+        }
+        else if ("p" == this.tagName.toLowerCase()) {
+            ++p;
+            id = 'p' + h1 + '.' + h2 + '_' + p;
+        }
+
+        $(this).attr('id', id);
+        $('<a>#</a>').prependTo(this).addClass('bookmark').attr('href', '#' + id);
+    });
 });
 
